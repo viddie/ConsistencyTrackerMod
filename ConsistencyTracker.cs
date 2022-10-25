3612bd668e986d53bb33eda43585773d5e5d2389
Hannes Ru <viddie@hotmail.de>
2022-10-25 13:20:41 +0200

    Updated version to 1.3.3

--- ConsistencyTracker.cs
+++ ConsistencyTracker.cs
@@ -15,7 +15,7 @@ namespace Celeste.Mod.ConsistencyTracker {
         private static readonly int LOG_FILE_COUNT = 10;
 
         public static readonly string OverlayVersion = "1.1.1";
-        public static readonly string ModVersion = "1.3.2";
+        public static readonly string ModVersion = "1.3.3";
 
         public override Type SettingsType => typeof(ConsistencyTrackerSettings);
         public ConsistencyTrackerSettings ModSettings => (ConsistencyTrackerSettings)this._Settings;
