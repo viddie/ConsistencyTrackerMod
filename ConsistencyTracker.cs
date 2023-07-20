39e21154640d23a11807d4d179fcb3c782acb15d
Hannes Ru <viddie@hotmail.de>
2023-07-20 14:47:51 +0200

    Updated version to 2.3.8

--- ConsistencyTracker.cs
+++ ConsistencyTracker.cs
@@ -27,7 +27,7 @@ namespace Celeste.Mod.ConsistencyTracker {
 
         #region Versions
         public class VersionsNewest {
-            public static string Mod => "2.3.7";
+            public static string Mod => "2.3.8";
             public static string Overlay => "2.0.0";
             public static string LiveDataEditor => "1.0.0";
             public static string PhysicsInspector => "1.1.2";
