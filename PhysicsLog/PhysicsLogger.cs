80b6a16f7c958dd5dfa4ab48893c6bda1e2e8a9b
Hannes Ru <viddie@hotmail.de>
2024-02-26 13:53:19 +0100

    Updated version to 2.7.0

--- PhysicsLog/PhysicsLogger.cs
+++ PhysicsLog/PhysicsLogger.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using Celeste.Mod.ConsistencyTracker.EverestInterop;
 
 namespace Celeste.Mod.ConsistencyTracker.PhysicsLog
 {
