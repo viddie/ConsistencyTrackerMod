d61b7302eed140abf1d90b78162b10906f7f24b2
viddie <viddie@hotmail.de>
2026-09-02 11:17:22 +0200

    Update version to 2.10.3

--- pack.sh
+++ pack.sh
@@ -1,1 +1,5 @@
+echo "Deleting old .zip if it exists..."
+rm ConsistencyTrackerMod.zip
+echo "Creating new archive..."
 zip -r ConsistencyTrackerMod.zip everest.yaml Assets Dialog bin/ConsistencyTracker.dll bin/ConsistencyTracker.pdb
+echo "Done"
